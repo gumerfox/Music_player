8f50569c053116cbd8a01a9933580e7de355dc0b
gumerfox <gumerov.vasil@gmail.com>
2022-11-22 14:14:18 +0500

    Закончил 3 день практики

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,43 @@
-/*import * as flsFunctions from './modules/functions.js'
+const audio = new Audio();
+const tracksCard = document.getElementsByClassName("track");
+const player = document.querySelector(".player");
+const pauseBtn = document.querySelector(".player__icon--pause");
+const stopBtn = document.querySelector(".player__icon--stop");
+// const activeBtn = document.querySelector(".track__img-wrap");
 
-flsFunctions.isWebp()
+const playMusic = (event) => {
+  const trackActive = event.currentTarget;
 
-import Swiper, { Navigation, Pagination } from 'swiper'
+  audio.src = trackActive.dataset.track;
+  audio.play();
+  pauseBtn.classList.remove("player__icon--play");
+  player.classList.add("player--active");
+  // activeBtn.classList.remove("track__img-wrap");
 
-const swiper = new Swiper()*/
+  for (let i = 0; i < tracksCard.length; i++) {
+    tracksCard[i].classList.remove("track--active");
+  }
 
-$(function(){
-    $('.header__btn').on('click', function(){
-        $('.rightside-menu').removeClass('rightside-menu--close');
-    });
-    $('.rightside-menu__close').on('click', function(){
-        $('.rightside-menu').addClass('rightside-menu--close');
-    });
+  trackActive.classList.add("track--active");
+};
+
+for (let i = 0; i < tracksCard.length; i++) {
+  tracksCard[i].addEventListener("click", playMusic);
+}
+
+pauseBtn.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+    pauseBtn.classList.remove("player__icon--play");
+  } else {
+    audio.pause();
+    pauseBtn.classList.add("player__icon--play");
+  }
+});
+
+stopBtn.addEventListener("click", () => {
+  if (audio.played) {
+    audio.pause();
+  }
+  player.classList.remove("player--active");
 });
